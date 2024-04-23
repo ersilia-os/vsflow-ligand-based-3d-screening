2b4422fc9c2684536417093edb2f83c44aa72f05
Miquel Duran-Frigola <miquelduranfrigola@gmail.com>
2024-04-23 19:35:42 +0200

    wip vsflow

--- src/vsflow3d.py
+++ src/vsflow3d.py
@@ -11,18 +11,42 @@ tmp_dir = tempfile.mkdtemp(prefix='vsflow3d-')
 def main() -> None:
     args = parseArgs()
 
+    query_file = args.query_file
+    database_file = args.database_file
+    output_folder = args.output_folder
+
+    cmd = f'vsflow3d shape -q {query_file} -d {database_file} -o {output_folder} -t 1000'
+
+    subprocess.Popen(cmd, shell=True).wait()
+
+    results_file = os.path.join(output_folder, 'results.csv')
+    
+    with open(output_folder, 'r') as f:
+        pass
+
+
 
 def parseArgs() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description='Generates conformers for a given list of SMILES strings.')
 
-    parser.add_argument('-i',
-                        dest='in_file',
+    parser.add_argument('-q',
+                        dest='query_file',
                         required=True,
                         metavar='<file>',
-                        help='Molecule input file in CSV format. A column named "smiles" is required.')
-
-
-
+                        help='Query molecule in SDF format. Multiple conformers are permitted.')
+    
+    parser.add_argument('-d',
+                        dest='database_file',
+                        required=True,
+                        metavar='<file>',
+                        help='Database molecules in SDF format. Multiple conformers are permitted.')
+    
+    parser.add_argument('-o',
+                        dest='output_folder',
+                        required=True,
+                        metavar='<folder>',
+                        help='Output folder.')
+    
     return parser.parse_args()
 
 
